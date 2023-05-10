54d60f10999b6620f389688fe932291ecefe42c4
onurd <106928484+onurdzd@users.noreply.github.com>
2023-05-10 14:38:50 +0300

    vercel serverless

---
+++ yaho-market/api/vercel-yaho-router.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+module.exports =async (req,res)=>{
+    if(req.method === "GET"){
+        let options = {
+            method: 'GET',
+            url: import.meta.env.VITE_VERCEL_URL,
+            params: {
+              q: req.params.data,
+              region: 'US'
+            },
+            headers: {
+              'X-RapidAPI-Key': import.meta.env.VITE_VERCEL_API,
+              'X-RapidAPI-Host': import.meta.env.VITE_VERCEL_HOST
+            }
+          };
+        
+          try {
+            let response = await axios.request(options);
+            if(response){
+                res.status(200).json({message:response.data.news})
+            }else{
+                res.status(400).json({message:"Cevap yok"})
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }else{
+        res.send({status:"get req yok"})
+    }
+}
--- yaho-market/src/Components/Arama.jsx
+++ yaho-market/src/Components/Arama.jsx
@@ -19,21 +19,39 @@ const Arama = ({ setResult ,darkMode,loading,setLoading,setHisseKodu,setIlkArama
     //   })
     //   .catch((err) => console.log(err));
 
-    let options = {
-      method: 'GET',
-      url: import.meta.env.VITE_VERCEL_URL,
-      params: {
-        q: data,
-        region: 'US'
-      },
-      headers: {
-        'X-RapidAPI-Key': import.meta.env.VITE_VERCEL_API,
-        'X-RapidAPI-Host': import.meta.env.VITE_VERCEL_HOST
-      }
-    };
+
+    //frontentden direk req:
+  //   let options = {
+  //     method: 'GET',
+  //     url: import.meta.env.VITE_VERCEL_URL,
+  //     params: {
+  //       q: data,
+  //       region: 'US'
+  //     },
+  //     headers: {
+  //       'X-RapidAPI-Key': import.meta.env.VITE_VERCEL_API,
+  //       'X-RapidAPI-Host': import.meta.env.VITE_VERCEL_HOST
+  //     }
+  //   };
   
+  //   try {
+  //     let response = await axios.request(options);
+  //     console.log("request atıldı",import.meta.env.VITE_VERCEL_HOST)
+  //     if(response){
+  //       setLoading(false)
+  //       setResult(response.data.news)
+  //     }else{
+  //       setLoading(false)
+  //       setResult([])
+  //     }
+  // } catch (error) {
+  //     console.error(error);
+  // }
+
+
+  //vercel için req:
     try {
-      let response = await axios.request(options);
+      let response = await axios.get(`/${data}`);
       console.log("request atıldı",import.meta.env.VITE_VERCEL_HOST)
       if(response){
         setLoading(false)
@@ -59,7 +77,7 @@ const Arama = ({ setResult ,darkMode,loading,setLoading,setHisseKodu,setIlkArama
     }`} onChange={(e) => setData(e.target.value)} value={data}></input>
         </label>
         <button type="submit" className="bg-orange-600 border-2 border-black rounded mr-2 p-2 font-semibold" onClick={()=>{setLoading(!loading);setHisseKodu("");setIlkArama(false)}}>Getir</button>
-        <button className="bg-lime-800 border-2 border-black rounded p-2 font-semibold" onClick={()=>{setHisseKodu("");setResult([]);setData("");setIlkArama(true)}}>Temizle</button>
+        <button type="button" className="bg-lime-800 border-2 border-black rounded p-2 font-semibold" onClick={()=>{setHisseKodu("");setResult([]);setData("");setIlkArama(true)}}>Temizle</button>
       </form>
     </div>
   );
