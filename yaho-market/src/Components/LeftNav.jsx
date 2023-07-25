79584d37f5be67ea78b16ee34c876f6fc7f965a7
onurd <106928484+onurdzd@users.noreply.github.com>
2023-07-25 15:36:29 +0300

    new currencies

--- yaho-market/src/Components/LeftNav.jsx
+++ yaho-market/src/Components/LeftNav.jsx
@@ -3,6 +3,7 @@ import axios from "axios"
 
 const LeftNav=()=>{
   const [marketData,setMarketData] =useState({})
+  const [marketData2,setMarketData2] =useState({})
 
 //localhost req:
 //     useEffect(() => {
@@ -35,7 +36,7 @@ const LeftNav=()=>{
 
     useEffect(() => {
     async function fetchMyAPI() {
-      let options2 = {
+      let options = {
         method: "GET",
         url: import.meta.env.VITE_MARKET_URL,
         params: {
@@ -48,18 +49,43 @@ const LeftNav=()=>{
           "X-RapidAPI-Host": import.meta.env.VITE_MARKET_HOST,
         },
       };
-        const response =await axios.request(options2);
+      let options2 = {
+        method: "GET",
+        url: import.meta.env.VITE_MARKET3_URL,
+        params: {
+          id: 'aud,brl,cad,chf,cny,czk,dkk,eur,gbp,hkd,huf,inr,jpy,mxn,nok,nzd,pln,rub,sek,try,twd,usd,zar'
+        },
+        headers: {
+          "X-RapidAPI-Key":import.meta.env.VITE_VERCEL_API,
+          "X-RapidAPI-Host": import.meta.env.VITE_MARKET2_HOST,
+        },
+      };
+
+      try {
+        const response =await axios.request(options);
         setMarketData(response.data);
+        const response2 = await axios.request(options2);
+        setMarketData2(response2.data);
+        console.log(marketData2.result)
+      } catch (error) {
+        console.error(error);
+      }
     }
     fetchMyAPI();
   }, []);
 
+  let exoticCurrencies=[]
+  for ( let i in marketData2.result){
+    exoticCurrencies.push({currency:marketData2.result[i].symbol,price:marketData2.result[i].last.slice(0,5)})
+  }
 
     return(
-        <div className="flex-1 text-center pt-2  max-w-[400px] mx-auto">
+        <div className="flex-1 text-center pt-2 max-w-[400px] mx-auto max-h-[600px] overflow-auto">
             <h2 className="text-yellow-500 text-xl font-serif">Market Data</h2>
-            <div className="text-yellow-600 text-l font-serif pt-5">EUR/USD:  {marketData["EUR/USD"] ? marketData["EUR/USD"].price :1.09510}</div>
-            <div className="text-yellow-600 text-l font-serif pt-2">NASDAQ:  { marketData["NDX"] ? marketData["NDX"].price :13291.291}</div>
+            <div className="text-blue-400 text-base font-serif pt-5">EURUSD:  {marketData["EUR/USD"] ? marketData["EUR/USD"].price.slice(0,5) :1.09510}</div>
+            <div className="text-blue-400 text-base font-serif ">NASDAQ:  { marketData["NDX"] ? marketData["NDX"].price.slice(0,5) :13291.291}</div>
+            {exoticCurrencies?.map((item,index)=>
+              <div className="text-blue-400 text-base font-serif" key={index}>{item.currency+" : " + item.price}</div>)}
         </div>
     )
 }
